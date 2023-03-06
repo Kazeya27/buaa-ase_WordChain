3c36fdd5c8fdc1ad0fb38778af460d10383c39e3
wzy2001wzy <864294516@qq.com>
2023-03-06 16:12:13 +0800

    jiekouxiewanle

--- src/src.cpp
+++ src/src.cpp
@@ -29,9 +29,10 @@ vector<string> input()
 	}
 	return res;	
 }
-vector<vector<int> > build_graph(int n,vector<string> str)
+vector<vector<int> > build_graph(vector<string> str)
 {
-	set<string> st;//unique
+	sort(str.begin(),str.end());
+	int n=unique(str.begin(),str.end())-str.begin();
 	vector<vector<int> > g(n,vector<int>());
 	for(int i=0;i<n;i++)
 	{
@@ -85,7 +86,7 @@ int op,char start,char end,set<char> ban)
 	for(int i=0;i<n;i++)
 		if(dp[i]>mx&&(end=='0'||str[i].back()==end))
 			mx=dp[i],pos=i;
-	cout<<mx<<" "<<pos<<"\n";
+	//cout<<mx<<" "<<pos<<"\n";
 	vector<string> res;
 	while(pos!=-1)
 	{
@@ -122,7 +123,10 @@ int check(int n,vector<vector<int> > g)
 	return cnt==n;
 }
 vector<vector<string> > all_list;
+vector<string> ans_circle;
 vector<string> now;
+vector<int> vis;
+int now_max,char_len;
 void get_all(int n,vector<vector<int> > g,vector<string> str,int x)
 {
 	if(x==-1)
@@ -143,6 +147,106 @@ void get_all(int n,vector<vector<int> > g,vector<string> str,int x)
 		now.pop_back();
 	}
 }
+void circle_max(int n,int x,vector<vector<int> > g,vector<string> str,
+int op,char start,char end,set<char> ban)
+{
+	//cout<<x<<" ";
+	if(x==-1)
+	{
+		for(int i=0;i<n;i++)
+		{
+			if(start!='0'&&start!=str[i][0]) continue;
+			now.push_back(str[i]);
+			char_len+=str[i].length();
+			circle_max(n,i,g,str,op,start,end,ban);
+			char_len-=str[i].length();
+			now.pop_back();
+		}
+		return;
+	}
+	if(vis[x]==1) return;
+	if(ban.find(str[x][0])!=ban.end()) return;
+	vis[x]=1;
+	int len=(op==0)?now.size():char_len;
+	if(len>max(now_max,1)) 
+	{
+		if(end=='0'||end==str[x].back())
+		{
+			now_max=len;
+			ans_circle.assign(now.begin(),now.end());
+		}
+	}
+	for(auto i:g[x])
+	{
+		now.push_back(str[i]);
+		char_len+=str[i].length();
+		circle_max(n,i,g,str,op,start,end,ban);
+		char_len-=str[i].length();
+		now.pop_back();
+	}
+	vis[x]=0;
+}
+int gen_chain_word(char* words[], int len, char* result[],int type, char head, char tail,char banned,bool enable_loop)
+{
+	vector<string> all_str;
+	for(int i=0;i<len;i++) all_str.push_back(words[i]);
+	vector<vector<int> > g=build_graph(all_str);
+	int sz=all_str.size();
+	set<char> ban;
+	if(banned!='0') ban.insert(banned);
+	if(enable_loop)
+	{
+		ans_circle.clear();
+		now_max=0;
+		now.clear();
+		vis=vector<int>(sz,0);
+		circle_max(sz,-1,g,all_str,type,head,tail,ban);
+		int cnt=0;
+		for(auto x:ans_circle)
+		{
+			result[cnt++]=(char *)x.data();
+		}
+		return 0;
+	}
+	else
+	{
+		if(!check(sz,g))
+		{
+			cout<<"fucked";
+			return -1;
+		}
+		vector<string> ans=get_max(sz,g,all_str,type,head,tail,ban);
+		int cnt=0;
+		for(auto x:ans)
+		{
+			result[cnt++]=(char *)x.data();
+		}
+		return 0;
+	}
+}
+int gen_chains_all(char* words[], int len, char* result[])
+{
+	vector<string> all_str;
+	for(int i=0;i<len;i++) all_str.push_back(words[i]);
+	vector<vector<int> > g=build_graph(all_str);
+	int sz=all_str.size();
+	if(!check(sz,g))
+	{
+		cout<<"fucked";
+		return -1;
+	}
+	all_list.clear();
+	get_all(sz,g,all_str,-1);
+	int cnt=0;
+	for(auto x:all_list)
+	{
+		string tmp="";
+		for(auto y:x) tmp+=y,tmp+=" ";
+		result[cnt++]=(char *)tmp.data();
+		cout<<"\n";
+	}
+	return 0;
+}
 void print_ans(vector<string> ans)
 {
 	cout<<ans.size()<<"\n";
@@ -161,17 +265,43 @@ void print_all()
 		cout<<"\n";
 	}
 }
-int main()
+int main(int argc,char* argv[])
 {
 	vector<string> all_str=input();
 	int sz=all_str.size();
-	vector<vector<int> > g=build_graph(sz,all_str);
+	vector<vector<int> > g=build_graph(all_str);
+	
 	
 	//test
 	char start='0',end='0',b[100]={'b'};
+	int op=0;
 	set<char> ban;
-	for(int i=0;i<26;i++) ban.insert(b[i]);
-	
+	for(int i=1;i<argc;i++)
+	{
+		if(argv[i]=="-w") op=0;
+		else if(argv[i]=="-c") op=1;
+		else if(argv[i]=="-h") 
+		{
+			i++;
+			start=argv[i][0];
+		}
+		else if(argv[i]=="-t")
+		{
+			i++;
+			end=argv[i][0];
+		}
+		else if(argv[i]=="-j")
+		{
+			i++;
+			ban.insert(argv[i][0]);
+		}
+	}
+	vis=vector<int>(sz,0);
+	circle_max(sz,-1,g,all_str,op,start,end,ban);
+	//vector<string> ans=get_max(sz,g,all_str,op,start,end,ban);
+	print_ans(ans_circle);
+	//test
+	/*
 	if(!check(sz,g))
 	{
 		cout<<"fucked";
@@ -185,6 +315,6 @@ int main()
 	cout<<"#####\n";
 	ans=get_max(sz,g,all_str,1,start,end,ban);
 	print_ans(ans);
-	
+	*/
 	
 }
