66fdd40f0485343cd5b277ecfee80a7d18027d44
王永瑶 <19374223@buaa.edu.cn>
2023-03-12 00:55:30 +0800

    单元测试

--- src/main.cpp
+++ src/main.cpp
@@ -1,168 +1,208 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+#include<fstream>
+#include<set>
+#include<string>
 using namespace std;
 ifstream readFile;
-// diao yong dll
-// shengming hanshu
 ofstream fout("output.txt");
+// diao yong dll
+#pragma comment(lib, "../bin/WordChain.lib")
+// shengming hanshu
+extern "C" __declspec(dllexport) int gen_chain_word(char* words[], int len, char* result[], char head, char tail, char banned, bool enable_loop);
+extern "C" __declspec(dllexport) int gen_chain_char(char* words[], int len, char* result[], char head, char tail, char banned, bool enable_loop);
+extern "C" __declspec(dllexport) int gen_chains_all(char* words[], int len, char* result[]);
+int isletter(char c)
+{
+    return (c>='a'&&c<='z')||(c>='A'&&c<='Z');
+}
+int issmall(char c)
+{
+    return (c>='a'&&c<='z');
+}
 vector<string> input()
 {
-	vector<string> res;
-	string s;
-	while(getline(readFile,s))
-	{
-		string now;
-		for(int i=0;i<s.size();i++)
-		{
-			if(!isletter(s[i]))
-			{
-				if(now.size()>0) res.push_back(now);
-				now="";
-			}
-			else now+=(s[i]<'a')?(s[i]+'a'-'A'):s[i];
-		}
-		if(now.size()>0) res.push_back(now);
-	}
-	return res;	
+    vector<string> res;
+    string s;
+    while(getline(readFile,s))
+    {
+        string now;
+        for(int i=0;i<s.size();i++)
+        {
+            if(!isletter(s[i]))
+            {
+                if(now.size()>0) res.push_back(now);
+                now="";
+            }
+            else now+=(s[i]<'a')?(s[i]+'a'-'A'):s[i];
+        }
+        if(now.size()>0) res.push_back(now);
+    }
+    return res;
 }
-void print_ans(vector<string> ans)
+int check_file(int argc,char* argv[])
 {
-	if(ans.size()<=1)
-	{
-		fout<<"no word chain\n";
-		return;
-	}
-	fout<<ans.size()<<"\n";
-	for(auto s:ans)
-		fout<<s<<"\n";
+    if(argc<2)
+    {
+        cout<<"too few para!";
+        return -9;
+    }
+    string fn=argv[argc-1];
+    if(fn.size()<=4||fn.substr(fn.size()-4)!=".txt")
+    {
+        cout<<"not .txt file!";
+        return -6;
+    }
+//    cout << argv[argc-1] << endl;
+    readFile.open(argv[argc-1], ios::in);
+    if(!readFile.is_open())
+    {
+        cout<<"open file failed!";
+        return -7;
+    }
+    return 0;
 }
-void print_all()
+int arg[10];
+int checkp(int x)
 {
-	if(all_list.size()<=1)
-	{
-		fout<<"no word chain\n";
-		return;
-	}
-	fout<<all_list.size()<<"\n";
-	for(auto x:all_list)
-	{
-		for(auto y:x)
-		{
-			fout<<y<<" ";
-		}
-		fout<<"\n";
-	}
+    if(x==1)
+    {
+        for(int i=1;i<=7;i++)
+            if(arg[i]==1) return -1;
+        return 0;
+    }
+    if(x==2||x==3)
+    {
+        if(arg[1]==1||arg[2]==1||arg[3]==1) return -1;
+        return 0;
+    }
+    if(arg[1]==1||arg[x]==1) return -1;
+    return 0;
 }
+int checkz(char* c)
+{
+    int len=strlen(c);
+    if(len!=1) return -4;
+    if(!issmall(c[0])) return -4;
+    return 0;
+}
+char start='\0',ed='\0',banned='\0';
+int op=-1,loop=0,qall=0;
+int read_para(int argc,char *argv[])
+{
+    for(int i=1;i<argc-1;i++)
+    {
+        string ag=argv[i];
+        if(ag=="-w")
+        {
+            if(checkp(1)<0) return -1;
+            arg[1]=1;
+            op=0;
+        }
+        else if(ag=="-c")
+        {
+            if(checkp(2)<0) return -1;
+            arg[2]=1;
+            op=1;
+        }
+        else if(ag=="-h")
+        {
+            if(checkp(3)<0) return -1;
+            arg[3]=1;
+            i++;
+            int res=checkz(argv[i]);
+            if(res<0) return res;
+            start=argv[i][0];
+        }
+        else if(ag=="-t")
+        {
+            if(checkp(4)<0) return -1;
+            arg[4]=1;
+            i++;
+            int res=checkz(argv[i]);
+            if(res<0) return res;
+            ed=argv[i][0];
+        }
+        else if(ag=="-j")
+        {
+            if(checkp(5)<0) return -1;
+            arg[5]=1;
+            i++;
+            int res=checkz(argv[i]);
+            if(res<0) return res;
+            banned=argv[i][0];
+        }
+        else if(ag=="-r")
+        {
+            if(checkp(6)<0) return -1;
+            arg[6]=1;
+            loop=1;
+        }
+        else if(ag=="-n")
+        {
+            cout << "模式-n";
+            if(checkp(7)<0) return -1;
+            arg[7]=1;
+            qall=1;
+        }
+        else
+        {
+            cout<<"wrong para!";
+            return -5;
+        }
+    }
+    if(op==-1) return -2;
+    return 0;
+}
+void print_ans(int len,char* result[],int op)
+{
+    int i;
+    if(op==1) fout<<len<<"\n";
+    for(int i=0;i<len;i++) cout<<result[i]<<"\n";
+}
+char* words[10010];
+char* result[10010];
+
 int main(int argc,char* argv[])
 {
-	if(argc<2)
-	{
-		cout<<"too few para!";
-		return -2;
-	}
-	readFile.open(argv[argc-1], ios::in);
-	if(!readFile.is_open())
-	{
-		cout<<"open file failed!";
-		return -3;
-	}
-	vector<string> all_str=input();
-	sort(all_str.begin(),all_str.end());
-	int sz=unique(all_str.begin(),all_str.end())-all_str.begin();
-	//print_ans(all_str);
-	vector<vector<int> > g=build_graph(all_str);
-	//char* word[10]={"aec","cef","fb","bdfsdfrrd","dear"};
-	//char* result[10];
-	//gen_chain_word_char(word,5,result,'0','0','0',0);
-	//test
-	char start='0',end='0',b[100]={'b'};
-	int op=-1,loop=0,qall=0;
-	set<char> ban;
-	//string cmd[10]={"-w","-c","-h","-t","-j","-r","-n"};
-	for(int i=1;i<argc-1;i++)
-	{
-		string ag=argv[i];
-		if(ag=="-w") 
-		{
-			if(op!=-1) 
-			{
-				cout<<"wrong para!";
-				return -3;
-			}
-			op=0;
-		}
-		else if(ag=="-c") 
-		{
-			if(op!=-1) 
-			{
-				cout<<"wrong para!";
-				return -3;
-			}
-			op=1;
-		}
-		else if(ag=="-h") 
-		{
-			i++;
-			start=argv[i][0];
-		}
-		else if(ag=="-t")
-		{
-			i++;
-			end=argv[i][0];
-		}
-		else if(ag=="-j")
-		{
-			i++;
-			ban.insert(argv[i][0]);
-		}
-		else if(ag=="-r") 
-		{
-			loop=1;
-		}
-		else if(ag=="-n") 
-		{
-			qall=1;
-		}
-		else
-		{
-			cout<<"wrong para!";
-			return -3;
-		}
-	}
-	if(qall==1)
-	{
-		init(sz);
-		get_all(sz,g,all_str,-1);
-		print_all();
-		return 0;
-	}
-	if(loop==0)
-	{
-		if(!check(sz,g))
-		{
-			cout<<"fucked";
-			return -1;
-		}	
-		vector<string> ans=get_max(sz,g,all_str,op,start,end,ban);
-		print_ans(ans);
-		return 0;
-	}
-	else
-	{
-		init(sz);
-		circle_max(sz,-1,g,all_str,op,start,end,ban);
-		print_ans(ans_circle);
-		return 0;
-	}
-	//test
-	/*
-	
-	
-	cout<<"#####\n";
-	vector<string> ans=get_max(sz,g,all_str,0,start,end,ban);
-	print_ans(ans);
-	cout<<"#####\n";
-	ans=get_max(sz,g,all_str,1,start,end,ban);
-	print_ans(ans);
-	*/
-	
+    int res=check_file(argc,argv);
+    if(res<0) return res;
+    res=read_para(argc,argv);
+    if(res<0) return res;
+
+    vector<string> all_str=input();
+    int len,reslen;
+    len=all_str.size();
+    for(int i=0;i<len;i++)
+    {
+        words[i]=new char[all_str[i].size()*sizeof(char)];
+        for(int j=0;j<all_str[i].size();j++)
+            words[i][j]=all_str[i][j];
+        words[i][all_str[i].size()]='\0';
+        //cout<<words[i]<<"#\n";
+    }
+    if(qall==1)
+    {
+        reslen=gen_chains_all(words,len,result);
+        if(reslen<0) return reslen;
+        print_ans(reslen,result,1);
+        return 0;
+    }
+    if(op==0)
+    {
+        reslen=gen_chain_word(words,len,  result,  start,  ed, banned,loop);
+        if(reslen<0) return reslen;
+        print_ans(reslen,result,0);
+        return 0;
+    }
+    else
+    {
+        reslen=gen_chain_char(words,len,  result,  start,  ed, banned,loop);
+        if(reslen<0) return reslen;
+        print_ans(reslen,result,0);
+        return 0;
+    }
+
 }
---
+++ test/gtest.cpp
@@ -0,0 +1,213 @@
+//
+// Created by Kazeya on 2023/3/11.
+//
+#include "gtest/gtest.h"
+#include "../src/core.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+
+void chain_test(char*result[], int len) {
+    for (int i = 0;i<len-1;i++) {
+        int len1 = strlen(result[i]);
+        ASSERT_EQ(result[i][len1-1],result[i+1][0]);
+    }
+}
+
+void chain_test_all(char*result[], int len) {
+    for (int i = 0;i<len;i++) {
+        char* s = result[i];
+        int len1 = strlen(s);
+        for (int j = 0;j<len1;j++) {
+            for (int k = j+1;k<len1;k++) {
+                if (s[k] == ' ') {
+                    ASSERT_EQ(s[k-1],s[k+1]);
+                    j = k + 1;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void head_tail_ban_test(char*result[],char head,char tail,char banned,int len) {
+    if (len > 0) {
+        if (head != '\0')
+            ASSERT_EQ(result[0][0],head);
+        if (tail != '\0') {
+            int str_len = strlen(result[len - 1]);
+            ASSERT_EQ(result[len-1][str_len - 1],tail);
+        }
+        if (banned != '\0') {
+            for (int i = 0;i<len;i++) {
+                ASSERT_NE(result[i][0],banned);
+            }
+        }
+    }
+}
+
+void answer_test(int index, char*words[], char*result[], int* rtn) {
+    int model;
+    char head = '\0',tail = '\0',banned = '\0';
+    bool enable_loop = false;
+    string fileName = "../test/testfiles/testfile" + to_string(index) + ".txt";
+    ifstream ifile;
+    ifile.open(fileName,ios::in);
+    if (!ifile.is_open()) {
+        cout << fileName + " open file failed" << endl;
+        return;
+    }
+    string line;
+    vector<char*> buffer;
+    while (getline(ifile, line))
+    {
+        if (!line.empty())
+        {
+            char* ptr = new char[line.length() + 1];
+            strcpy_s(ptr, line.length() + 1, line.c_str());
+            switch (ptr[0]) {
+                case 'n':
+                    model = 0;
+                    break;
+                case 'w':
+                    model = 1;
+                    break;
+                case 'c':
+                    model = 2;
+                    break;
+                case 'h':
+                    head = ptr[2];
+                    break;
+                case 't':
+                    tail = ptr[2];
+                    break;
+                case 'j':
+                    banned = ptr[2];
+                    break;
+                case 'r':
+                    enable_loop = true;
+                    break;
+            }
+        }
+        else
+            break;
+    }
+    while (getline(ifile, line))
+    {
+        if (!line.empty())
+        {
+            char* ptr = new char[line.length() + 1];
+            strcpy_s(ptr, line.length() + 1, line.c_str());
+            for (int i = 0; i < line.length(); i++)
+            {
+                ptr[i] += ('A' <= ptr[i] && ptr[i] <= 'Z') ? 'a' - 'A' : 0;
+            }
+            buffer.push_back(ptr);
+        }
+    }
+    int len = (int)buffer.size() - 1;
+    for (int i = 0; i < len; i++)
+    {
+        words[i] = buffer[i];
+    }
+    int realRtn = stoi(buffer[len]);
+    int tmp = -1;
+//    cout << head << " " << tail << " " << banned << endl;
+    switch (model) {
+        case 0:
+            tmp = gen_chains_all(words,len,result);
+            break;
+        case 1:
+            tmp = gen_chain_word(words,len,result,head,tail,banned,enable_loop);
+            break;
+        case 2:
+            tmp = gen_chain_char(words,len,result,head,tail,banned,enable_loop);
+            break;
+        default:
+            break;
+    }
+    *rtn = tmp;
+//    for (int i = 0;i<tmp;i++)
+//        cout << result[i] << endl;
+    ASSERT_EQ(tmp,realRtn);
+    if (model == 0) {
+        chain_test_all(result,*rtn);
+    }
+    else {
+        chain_test(result,*rtn);
+        head_tail_ban_test(result,head,tail,banned,len);
+    }
+}
+
+void unique_test(char*result[],int len) {
+    unordered_set<string> unique;
+    for (int i = 0;i<len;i++) {
+        ASSERT_TRUE(unique.find(result[i]) == unique.end());
+        unique.insert(result[i]);
+    }
+}
+
+TEST(TestCase,AllNorm){
+    char* result[20002];
+    char* words[20002];
+    int rtn;
+    answer_test(1, words, result, &rtn);
+    unique_test(result,rtn);
+}
+
+TEST(TestCase,AllEmpty){
+    char* result[20002];
+    char* words[20002];
+    int rtn;
+    answer_test(2, words, result, &rtn);
+    unique_test(result,rtn);
+}
+
+TEST(TestCase,WordNorm){
+    char* result[20002];
+    char* words[20002];
+    int rtn;
+    answer_test(3, words, result, &rtn);
+    unique_test(result,rtn);
+}
+
+TEST(TestCase,WordEmpty){
+    char* result[20002];
+    char* words[20002];
+    int rtn;
+    answer_test(4, words, result, &rtn);
+    unique_test(result,rtn);
+}
+
+TEST(TestCase,WordHead){
+    char* result[20002];
+    char* words[20002];
+    int rtn;
+    answer_test(5, words, result, &rtn);
+    unique_test(result,rtn);
+}
+
+TEST(TestCase,WordTail){
+    char* result[20002];
+    char* words[20002];
+    int rtn;
+    answer_test(6, words, result, &rtn);
+    unique_test(result,rtn);
+}
+
+TEST(TestCase,WordBan){
+    char* result[20002];
+    char* words[20002];
+    int rtn;
+    answer_test(7, words, result, &rtn);
+    unique_test(result,rtn);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();  // //RUN_ALL_TESTS()运行所有测试案例
+}
